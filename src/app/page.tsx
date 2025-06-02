2c78545546232495b1fff49d282bd033fc9aadec
Abdi-01 <abdialghi@gmail.com>
2025-06-02 20:26:43 +0700

    use global state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+import { useAppSelector } from "@/lib/redux/hook";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
+  const authUser = useAppSelector((state) => {
+    return state.authUserReducer;
+  })
+  console.log(authUser);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-semibold font-[family-name:var(--font-geist-mono)]">
-          Welcome, to Todo App
+          Welcome{authUser.firstname ? ` ${authUser.firstname}` : ""}, to Todo App
         </h1>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
@@ -14,12 +21,29 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">Check the todo is done</li>
         </ol>
-        <Link
-          className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-          href="/todo"
-        >
-          Type Todo
-        </Link>
+        {
+          authUser.objectId ?
+            <Link
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+              href="/todo"
+            >
+              Type Todo
+            </Link>
+            : <div className="flex gap-4">
+              <Link
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+                href="/signin"
+              >
+                Signin
+              </Link>
+              <Link
+                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+                href="/sign-up"
+              >
+                Sign Up
+              </Link>
+            </div>
+        }
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
